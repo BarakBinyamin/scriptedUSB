2b422fcbbe5d734354ec3eea0f9f2ed7eb623d2b
rock <barak.binyamin555@gmail.com>
2022-04-29 13:45:36 -0400

    updated heathers script

--- scripts/bash/Heather.sh
+++ scripts/bash/Heather.sh
@@ -1,1 +1,15 @@
 curl -s https://barakbinyamin.github.io/scriptedUSB/img/forHeather.txt
+
+curl https://someresources.github.io/songs/boss.m4a -o boss.m4a
+open boss.m4a --hide
+
+#set general volume
+osascript -e "set volume 3"
+
+for i in {1..15}; do
+	volume=`echo "$(( i / 10)).$(( i % 10))"`
+
+	#set itunes volume
+	osascript -e "tell application \"Music\" to set sound volume to $volume"
+	sleep .25
+done
