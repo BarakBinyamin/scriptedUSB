c4aa12d6f6cdaa8402b387a7e8fd89f18ed1dab6
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2023-08-07 11:54:35 -0400

    Updated comments to reference fix to macOS gcc error

--- arduinoSketches/autoHIDexample/autoHIDexample.ino
+++ arduinoSketches/autoHIDexample/autoHIDexample.ino
@@ -10,7 +10,7 @@
  *  5. Select upload first, then plug in your digispark within 60 seconds of pressing upload (the arrow in the top left)
  * 
  * To get the digispark usb working automatically for mac without keyboard setup:
- *  1. sudo find / -type d -name "digistump" in your terminal
+ *  1. sudo find / -type d -name "digistump" in your terminal # on linux, on mac lookup PATHUNITL=~/Library/Arduino15/packages
  *  2. Open /PATHUNITL/digistump/hardware/avr/1.6.7/libraries/DigisparkKeyboard/usbconfig.h
  *  3. Find the secction "--- Device Description ---"
  *  4. Replace the vendor id with an apple vendor id replace '0xNNN, 0xNNN' with "0xac, 0x05"
@@ -18,13 +18,15 @@
  * For mac reference https://null-byte.wonderhowto.com/how-to/hack-macos-with-digispark-ducky-script-payloads-0198555/
  * 
  * For CHAR codes reference https://github.com/digistump/DigistumpArduino/blob/master/digistump-avr/libraries/DigisparkKeyboard/DigiKeyboard.h
- * 
+ *
  * This Keyboard Checker https://keyboardchecker.com/ might be helpful resource
  * 
  * MOD_ keys  or "modifiers" have to be passed as a second parameter. they can be 
  * combined with an OR
  *  For example "ctrl+alt+t": DigiKeyboard.sendKeyStroke(KEY_T, MOD_ALT_LEFT | MOD_CONTROL_LEFT)
  *  
+ * For Resolving GCC error on MacOS Monterary: https://forum.arduino.cc/t/avr-g-bad-cpu-type-in-executable/595790/14
+ *
  */
 bool ran = false;
 
